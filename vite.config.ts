7f82e8ae11be8c450f334e2def4838392a1b663b
Bertrand KAFANDO <bertrandkafando07@gmail.com>
2026-02-03 20:41:28 +0100

    Fix GitHub Pages base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/skayanko-sketch.github.io/" : "/",
+  base: "/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
